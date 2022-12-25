b24af0c73026319aed258e41f7eb64a99eba9aa6
nthY <n_hat.yalcin@outlook.com>
2022-12-25 03:15:47 +0300

    HomeController test and style arrangement

---
+++ src/test/java/pizzas/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package pizzas;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)   // Web test for HomeController
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;    // injects MockMvc
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        CoreMatchers.containsString("Welcome to...")));
+    }
+}
