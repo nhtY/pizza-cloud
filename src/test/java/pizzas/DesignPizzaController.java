cc222f9fbda4d15da11885a83036cbdd45a561d0
nthY <n_hat.yalcin@outlook.com>
2022-12-26 19:55:18 +0300

    prepare DesignPizzaController

--- src/test/java/pizzas/DesignPizzaController.java
+++ src/test/java/pizzas/DesignPizzaController.java
@@ -1,4 +1,43 @@
 package pizzas;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("pizzaOrder")
 public class DesignPizzaController {
+
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+
+    }
+
+    @ModelAttribute(name = "pizzaOrder")
+    public PizzaOrder order() {
+        return new PizzaOrder();
+    }
+
+    @ModelAttribute(name = "pizza")
+    public Pizza pizza() {
+        return new Pizza();
+    }
+
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type ) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
 }
