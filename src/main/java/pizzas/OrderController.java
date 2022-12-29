71822d0bb36ede0f6c431906ab931462528454a9
nthY <n_hat.yalcin@outlook.com>
2022-12-30 00:58:16 +0300

    handle POSTed pizza orders

--- src/main/java/pizzas/OrderController.java
+++ src/main/java/pizzas/OrderController.java
@@ -3,8 +3,10 @@ package pizzas;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -14,5 +16,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(PizzaOrder pizzaOrder,
+                               SessionStatus sessionStatus){
+        log.info("Order taken {}", pizzaOrder);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
