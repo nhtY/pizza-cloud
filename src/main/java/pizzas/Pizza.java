c8d6d4cdf880e68eaf02ee53ab45fd15451fdbca
nthY <n_hat.yalcin@outlook.com>
2023-01-27 18:03:57 +0300

    adapt the domain for database persistence

--- src/main/java/pizzas/Pizza.java
+++ src/main/java/pizzas/Pizza.java
@@ -4,16 +4,22 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Pizza {
-    /* user defined pizza name */
+
+    /** identity of the pizza **/
+    private Long id;
+    private Date createdAt = new Date();
+
+    /** user defined pizza name **/
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    /* Ingredients of the pizza */
+    /** Ingredients List of the pizza **/
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
--- src/main/java/pizzas/PizzaOrder.java
+++ src/main/java/pizzas/PizzaOrder.java
@@ -6,37 +6,50 @@ import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class PizzaOrder {
+public class PizzaOrder implements Serializable {
 
-    // delivery address data
+    /** The name of the person who will receive the order **/
     @NotBlank(message = "Teslim alacak kişinin adı gereklidir")
     private String deliveryName;
 
+    /** Street info of the delivery address **/
     @NotBlank(message = "Cadde-Sokak bilgisi gereklidir")
     private String deliveryStreet;
 
+    /** City info of the delivery address **/
     @NotBlank(message = "Şehir bilgisi gereklidir")
     private String deliveryCity;
 
+    /** Zip code of the delivery address **/
     @NotBlank(message = "Zip kodu gereklidir")
     private String deliveryZip;
 
     // credit card data
+
+    /** Credit Card number **/
     @CreditCardNumber(message = "Geçersiz Kart numarası")
     private String ccNumber;
 
+    /** Credit Card expiration date MM/YY **/
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="MM/YY formatında olmalıdır")
     private String ccExpiration;
 
+    /** Credit Card CVV**/
     @Digits(integer = 3, fraction = 0, message = "Geçersiz CVV")
     private String ccCVV;
 
+    /** List of ordered pizzas **/
     private List<Pizza> pizzas = new ArrayList<>();
 
+    /**
+     * When a pizza is designed add it into the order list.
+     * @param pizza the designed Pizza object.
+     */
     public void addPizza (Pizza pizza) {
         this.pizzas.add(pizza);
     }
