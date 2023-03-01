eab454acaccfc0e045532153beba24459992ee2d
nthY <n_hat.yalcin@outlook.com>
2023-03-01 22:54:45 +0300

    create UserDetailsService bean

--- src/main/java/pizzas/security/SecurityConfig.java
+++ src/main/java/pizzas/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package pizzas.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import pizzas.User;
+import pizzas.data.UserRepository;
 
 @Configuration
 public class SecurityConfig {
@@ -13,5 +17,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+          User user = userRepo.findByUsername(username);
+            if (user != null)
+                return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
+    }
 }
